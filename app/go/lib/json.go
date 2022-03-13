dd33d1180b712e3a3096f55cabe1c01b6770c06b
okeysea <okeysea000@gmail.com>
2022-03-13 23:54:26 +0900

    add json decode from io.Reader

--- app/go/lib/json.go
+++ app/go/lib/json.go
@@ -3,6 +3,8 @@ package lib
 import (
 	"net/http"
 	"encoding/json"
+	"errors"
+	"io"
 )
 
 func JsonDecodeRequestBody(r *http.Request, v interface{}) {
@@ -10,4 +12,14 @@ func JsonDecodeRequestBody(r *http.Request, v interface{}) {
 
 	err := decoder.Decode(v)
 	ErrorCheck(err)
+}
+
+func JsonDecodeFromStream(body io.Reader) (interface{}, error) {
+	var v interface{}
+
+	if err := json.NewDecoder(body).Decode(&v); err != nil {
+		return nil, errors.New("unknown json unmarshal error")
+	}
+
+	return v, nil
 }
