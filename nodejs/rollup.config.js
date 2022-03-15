bdb5df1f592aa517cb09015f16dd40bd7422e762
okeysea <okeysea000@gmail.com>
2022-03-16 01:33:53 +0900

    add rollup, react, typescript, storybook

---
+++ nodejs/rollup.config.js
@@ -0,0 +1,42 @@
+import merge from 'deepmerge';
+import path from 'path';
+import { createBasicConfig } from '@open-wc/building-rollup';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import typescript from 'rollup-plugin-typescript2';
+import postcss from 'rollup-plugin-postcss';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+
+// TODO: プロダクション環境の場合の対応
+const baseConfig = createBasicConfig({developmentMode: true});
+
+export default merge(baseConfig, {
+  //input: './out-tsc/src/index.js',
+  input: './src/index.tsx',
+  output: {
+    dir: 'dist',
+    format: 'iife',
+    sourceMap: true,
+  },
+  plugins: [
+    serve({
+      open: true,
+      verbose: true,
+      contentBase: ['','public'],
+      host: '0.0.0.0',
+      port: 8082
+    }),
+    livereload({
+      watch: "dist",
+      port: 8090
+    }),
+    typescript(),
+    postcss({ plugins: [] }),
+    commonjs(),
+    replace({
+      // TODO: プロダクション環境の場合の対応
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    })
+  ]
+});
